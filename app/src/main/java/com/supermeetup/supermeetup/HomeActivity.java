87951ac82444c83e8b85c4ea416165f3d314e26a
Hengstar Gong <gongxinheng@gmail.com>
2017-10-04 23:16:45 -0700

    Added Crashlytics support

--- app/src/main/java/com/supermeetup/supermeetup/HomeActivity.java
+++ app/src/main/java/com/supermeetup/supermeetup/HomeActivity.java
@@ -1,13 +1,16 @@
 package com.supermeetup.supermeetup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_home);
     }
 }
